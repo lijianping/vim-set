5eb9fff207d67cb5e482f9f7cc8d133e6426b337
japin <japin@ubuntu.(none)>
2013-08-19 22:31:00 +0800

    add cscope plugin

--- install.sh
+++ install.sh
@@ -1,11 +1,23 @@
 #!/bin/bash 
 
-if [ ! -e /usr/local/bin/ctag ]; then
+if [ ! -e /usr/local/bin/ctags ]; then
     echo "Start Install Ctags..."
     tar zxvf ctags-5.8.tar.gz
     cd ctags-5.8  
     ./configure && make
     sudo make install 
+	cd ..
+fi
+
+# Install cscope plugin
+if [ ! -e /usr/local/bin/cscope ]; then
+	sudo apt-get install libncurses5-dev
+	echo "Install cscope plugin ..."
+	tar zxvf cscope-15.8a.tar.gz
+	cd cscope-15.8a 
+	./configure && make
+	sudo make install
+	cd ..
 fi
 
 echo "cp .vim to $HOME "
