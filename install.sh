aa035e7dd047c76960c556e4fcce53c18a23d3f0
japin <japin@ubuntu.(none)>
2013-08-19 20:27:32 +0800

    Is the catgs exists.

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash 
 
-echo "Start Install Ctags..."
-tar zxvf ctags-5.8.tar.gz
-cd ctags-5.8  
-./configure && make
-
-sudo make install 
+if [ ! -e /usr/local/bin/ctag ]; then
+    echo "Start Install Ctags..."
+    tar zxvf ctags-5.8.tar.gz
+    cd ctags-5.8  
+    ./configure && make
+    sudo make install 
+fi
 
 echo "cp .vim to $HOME "
 cp -r .vim $HOME
