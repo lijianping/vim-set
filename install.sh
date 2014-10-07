23cc4514070c5a8e1866c5108556341968ebade3
japin <itisabig@hotmail.com>
2014-10-07 20:44:01 +0800

    rebuild vim-set

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 #!/bin/bash 
 
 if [ ! -e /usr/local/bin/ctags ]; then
-    echo "Start Install Ctags..."
-    tar zxvf ctags-5.8.tar.gz
-    cd ctags-5.8  
-    ./configure && make
-    sudo make install 
+	echo "Start Install Ctags..."
+	tar zxvf ctags-5.8.tar.gz
+	cd ctags-5.8  
+	./configure && make 
+	sudo make install 
 	cd ..
+	rm -rf ctags-5.8
 fi
 
 # Install cscope plugin
@@ -18,10 +19,11 @@ if [ ! -e /usr/local/bin/cscope ]; then
 	./configure && make
 	sudo make install
 	cd ..
+	rm -rf cscope-15.8a
 fi
 
 echo "cp .vim to $HOME "
-cp -r .vim $HOME
+cp -r vim $HOME/.vim
 
 echo "cp .vimrc to $HOME "
-cp .vimrc $HOME
+cp vimrc $HOME/.vimrc
